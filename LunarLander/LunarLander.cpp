b6aa7379e14b309328922a0d3b6ae580dac5626c
Miron Bury <s1906443@glos.ac.uk>
2019-11-03 21:38:44 +0000

    test console coordinates

--- LunarLander/LunarLander.cpp
+++ LunarLander/LunarLander.cpp
@@ -1,20 +1,76 @@
-// LunarLander.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+#include <windows.h>
 
+#include <string>
 #include <iostream>
+
+static constexpr unsigned int FONT_WIDTH = 8;
+static constexpr unsigned int FONT_HEIGHT = 8;
+
+static constexpr unsigned int WINDOW_WIDTH = 100;
+static constexpr unsigned int WINDOW_HEIGHT = 50;
+
+int Error(const std::string &msg, int value = 1)
+{
+	std::cerr << msg << std::endl;
+	return value;
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	HANDLE hConsoleOut;
+
+	hConsoleOut = GetStdHandle(STD_OUTPUT_HANDLE);
+	if (hConsoleOut == INVALID_HANDLE_VALUE)
+		return Error("failed to open console output");
+
+	//std::cout << "_/|\\_";
+
+	// Set font size
+	CONSOLE_FONT_INFOEX cfi;
+	cfi.cbSize = sizeof(cfi);
+	cfi.nFont = 0;
+	cfi.dwFontSize.X = FONT_WIDTH;
+	cfi.dwFontSize.Y = FONT_HEIGHT;
+	cfi.FontFamily = FF_DONTCARE;
+	cfi.FontWeight = FW_NORMAL;
+	wcscpy_s(cfi.FaceName, L"Consolas");
+	if (!SetCurrentConsoleFontEx(hConsoleOut, FALSE, &cfi))
+		return Error("Fail to set font");
+
+	SMALL_RECT windowRect = {0, 0, WINDOW_WIDTH - 1, WINDOW_HEIGHT - 1};
+	if (SetConsoleWindowInfo(hConsoleOut, TRUE, &windowRect) == FALSE)
+		return Error("failed to set window size");
+
+	// Clear screen
+	CHAR_INFO screen[WINDOW_HEIGHT * WINDOW_WIDTH];
+	for (int y = 0; y < WINDOW_HEIGHT; y++)
+	{
+		for (int x = 0; x < WINDOW_WIDTH; x++)
+		{
+			int offset = x + y * WINDOW_WIDTH;
+			screen[offset].Char.UnicodeChar = ' ';
+			screen[offset].Attributes = 
+				FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE | FOREGROUND_INTENSITY | //foreground flags
+				0; //background flags
+		}
+	}
+
+	// test coodinates
+	int offset;
+	offset = 0 + 0 * WINDOW_WIDTH;
+	screen[offset].Char.UnicodeChar = 'A';
+	offset = WINDOW_WIDTH-1 + 0 * WINDOW_WIDTH;
+	screen[offset].Char.UnicodeChar = 'B';
+	offset = 0 + (WINDOW_HEIGHT-1) * WINDOW_WIDTH;
+	screen[offset].Char.UnicodeChar = 'C'; 
+	offset = (WINDOW_WIDTH-1) + (WINDOW_HEIGHT-1) * WINDOW_WIDTH;
+	screen[offset].Char.UnicodeChar = 'D';
+
+	// Display screen in the console
+	WriteConsoleOutput(hConsoleOut, screen, { WINDOW_WIDTH, WINDOW_HEIGHT }, {0, 0}, &windowRect);
+
+	// Wait forever
+	for (;;) {}
+
+	return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started:
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
